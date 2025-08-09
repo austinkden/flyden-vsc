30b5f47dc34d554dd01dacaeb17f859c2378185b
austinkden <austinstrong500@gmail.com>
2025-08-09 17:36:28 -0600

    Airline selection still needs search. Submit button doesn't function. Rest works

--- main.js
+++ main.js
@@ -15,3 +15,21 @@ startButton.addEventListener("click", () => {
       }, 400);
    }, 400);
 });
+
+const airlineListItems = document.querySelectorAll("section.airline div.list div.airline");
+
+airlineListItems.forEach(airline => {
+   airline.addEventListener("click", () => {
+      if (airline.classList.contains("selected")) {
+         document.querySelector("section.airline button.submit").classList.remove("allowed");
+         airline.classList.remove("selected");
+      } else {
+         document.querySelectorAll("section.airline div.list div.airline").forEach(airline => {
+            airline.classList.remove("selected");
+         });
+
+         airline.classList.add("selected");
+         document.querySelector("section.airline button.submit").classList.add("allowed");
+      }
+   });
+});
